f2b1cd1a4f7b5edaddcf8038c6da565cae90bef2
RuslanKravchuk1990 <meladzeruslan164@gmail.com>
2022-12-23 22:39:41 +0200

    First commit

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +21,14 @@ print('BASE_DIR', BASE_DIR)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--zzrt6)2w39w)if2!uo&@*rkktxb$iaqs!qb=aj&$#1yg_0bjt'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure--zzrt6)2w39w)if2!uo&@*rkktxb$iaqs!qb=aj&$#1yg_0bjt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1"  # 1 == True
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -85,6 +87,32 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+POSTGRES_DB = os.environ.get("POSTGRES_DB") #database name
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD") #database user password
+POSTGRES_USER = os.environ.get("POSTGRES_USER") #database username
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST") #database host
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT") #database port
+
+POSTGRES_READY = (
+    POSTGRES_DB is not None
+    and POSTGRES_PASSWORD is not None
+    and POSTGRES_USER is not None
+    and POSTGRES_HOST is not None
+    and POSTGRES_PORT is not None
+)
+
+if POSTGRES_READY:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": POSTGRES_DB,
+            "USER": POSTGRES_USER,
+            "PASSWORD": POSTGRES_PASSWORD,
+            "HOST": POSTGRES_HOST,
+            "PORT": POSTGRES_PORT,
+        }
+    }
 
 
 # Password validation
